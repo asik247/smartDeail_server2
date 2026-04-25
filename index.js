ceb4c8e2cf9712ef02a6e7feb517c20da4134cea
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-25 23:39:39 +0600

    latest product get using sort,limit method

--- index.js
+++ index.js
@@ -31,24 +31,65 @@ async function run() {
         //?connect the client to the server;
         await client.connect();
         const myDB = client.db("smartDeails2");
-        const myColl = myDB.collection("usersInfo2");
+        const usersColl = myDB.collection("usersInfo2");
+        //?products2 coll;
+        const productsColl = myDB.collection('products2');
+        //Todo:product get method code hre;
+        app.get('/products2',async(req,res)=>{
+            const email = req.query.email;
+            // console.log(email);
+            const query = {};
+            if(email){
+                query.email = email
+            }
+            const cursor = productsColl.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        //! Latest Products get;
+        app.get('/latestProducts2',async(req,res)=>{
+            const latestPro =productsColl.find().sort({created_at:-1}).limit(6)
+            const result = await latestPro.toArray();
+            res.send(result);
+        })
+        //? id use get specifique product get;
+        app.get('/products2/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:new ObjectId(id)};
+            const result = await productsColl.findOne(query);
+            res.send(result)
+        })
         //Todo:post userData in db;
         app.post('/usersInfo2', async (req, res) => {
             const allUserData = req.body;
 
             try {
-                const result = await myColl.insertOne(allUserData); 
+                const result = await usersColl.insertOne(allUserData); 
                 res.send(result); 
             } catch (error) {
                 console.error(error);
                 res.status(500).send({ message: 'Insert failed' });
             }
         });
-        //Todo:Gel specifique usersInfo;
+        //Todo:Get specifique usersInfo;
         app.get('/usersInfo2/:id',async(req,res)=>{
             const id = req.params.id;
             const query = {_id:new ObjectId(id)};
-            const result = await myColl.findOne(query);
+            const result = await usersColl.findOne(query);
+            res.send(result)
+        })
+        //Todo:Patch/update userInfo;
+        app.patch('/usersInfo2/:id',async(req,res)=>{
+            const id = req.params.id;
+            const newUserInfo = req.body;
+            const query = {_id:new ObjectId(id)};
+            const updatData = {
+                $set:{
+                    name:newUserInfo.name,
+                    email:newUserInfo.email
+                }
+            }
+            const result = await usersColl.updateOne(query,updatData);
             res.send(result)
         })
 
